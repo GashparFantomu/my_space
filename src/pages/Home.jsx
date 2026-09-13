8ecb0eae1aee3f49ea594bfd14dcc55ebc87805e
gashpar_fantomu <samcelmare@gmail.com>
2026-09-13 13:56:54 +0300

    frontend sucks ass ma mut pe cachyOS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
-import {Instagram} from "@mui/icons-material";
 
 function Home() {
     return (
@@ -21,7 +20,7 @@ function Home() {
             <Navbar/>
 
             <Box component="main" sx={{flexGrow: 1, p:3}}>
-                <Grid container spacing={3}>
+                <Grid container spacing={3} alignItems="stretch">
                     {/*c1*/}
                     <Grid item xs={12} md={4}>
                         <Box sx={{flexDirection: 'column', display: 'flex', gap: 3}}>
@@ -34,13 +33,35 @@ function Home() {
                                     padding: '24px',
                                 }}>
 
-                                <Box sx={{display: 'flex', flexDirection: {xs: 'column', md: 'row'}, alignItems: {xs: 'center', md: 'flex-start'}, gap: 2}}>
-                                    <Box sx={{ textAlign: { xs: 'center', md: 'center' } }}>
+                                <Box sx={{ flexDirection: {xs: 'column', md: 'row'}, alignItems: {xs: 'center', md: 'center'}, gap: 2}}>
+                                    <Box sx={{ textAlign: { xs: 'center', md: 'center' }, justifyContent: 'center' }}>
                                         <Typography variant="h4">
                                             personal space 1 nenea nenea
                                         </Typography>
                                         <Typography variant="subtitle1">
                                             idk how you got here, but thanks for dropping by :D
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </Paper>
+                            <Paper
+                                elevation={3}
+                                sx={{
+                                    display: 'flex',
+                                    backgroundColor: "rgba(31, 99, 135, 0.169)",
+                                    color: 'white',
+                                    borderRadius: '16px',
+                                    padding: '24px',
+                                }}>
+                                <Box sx={{display: 'flex', flexDirection: {xs: 'column', md: 'row'}, alignItems: {xs: 'center', md: 'center'}, gap: 2
+                                }}>
+                                    <Avatar sx={{width: 150, height: 150}}  alt="stai sa se incarce poza..." src="/profile1.jpg"></Avatar>
+                                    <Box sx={{ textAlign: { xs: 'center', md: 'left' } }}>
+                                        <Typography variant="h4">
+                                            nenea nenea nenea nenea
+                                        </Typography>
+                                        <Typography variant="subtitle1">
+                                            nenea NENEA
                                         </Typography>
                                     </Box>
                                 </Box>
@@ -53,37 +74,22 @@ function Home() {
                                 padding: '24px'
                             }}>
                                 <Box sx={{textAlign: { xs: 'center', md: 'center' }}}>
-                                    <Typography variant="h6">Card suplimentar de test in Coloana 1 dar in mijloc</Typography>
-                                </Box>
-                            </Paper>
-
-                            <Paper elevation={3} sx={{
-                                backgroundColor: "rgba(31, 99, 135, 0.169)",
-                                color: 'white',
-                                borderRadius: '16px',
-                                padding: '24px'
-                            }}>
-                                <Box sx={{textAlign: { xs: 'center', md: 'center' }}}>
                                     <Typography variant="h4">Hall of fame for my projects</Typography>
                                 </Box>
-                                <Box sx={{textAlign: { xs: 'center', md: 'center' }}}>
-                                    <Button variant="outlined">
+                                <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+                                    <Button variant="outlined" sx={{borderRadius: '16px'}}>
                                         <Typography variant="subtitle1">
-                                            project
+                                            nothing 1
                                         </Typography>
                                     </Button>
-                                </Box>
-                                <Box sx={{textAlign: { xs: 'center', md: 'center' }}}>
                                     <Button variant="outlined">
                                         <Typography variant="subtitle1">
-                                            project 1
+                                            nothing 2
                                         </Typography>
                                     </Button>
-                                </Box>
-                                <Box sx={{textAlign: { xs: 'center', md: 'center' }}}>
                                     <Button variant="outlined">
                                         <Typography variant="subtitle1">
-                                            project 1 - some hullshi-
+                                            project 3 - some hullshi-
                                         </Typography>
                                     </Button>
                                 </Box>
@@ -99,7 +105,7 @@ function Home() {
                                     <Typography variant="h4">
                                         My Socials
                                     </Typography>
-                                    <Stack direction="row" spacing={3}>
+                                    <Stack direction="row" spacing={3} sx={{alignItems: 'center', justifyContent: 'center'}}>
                                         <IconButton component="a"
                                                     href="https://github.com/GashparFantomu"
                                                     target="_blank"
@@ -128,68 +134,59 @@ function Home() {
                                                     sx={{color: 'white', backgroundColor: 'rgba(255, 255, 255, 0.08)', padding: '12px'}}>
                                             <YouTubeIcon sx={{fontSize: 26}} />
                                         </IconButton>
-
                                     </Stack>
                                 </Box>
+                            </Paper>
+                        </Box>
+                    </Grid>
 
+                    <Grid item xs={12} md={4}>
+                        <Box sx={{display: 'flex', flexDirection: 'column', gap: 3, width: '100%'}}>
 
+                            <Paper elevation={3} sx={{
+                                backgroundColor: "rgba(31, 99, 135, 0.169)",
+                                color: 'white',
+                                borderRadius: '16px',
+                                padding: '24px',
+                                flexGrow: 1,
+                                alignItems: {xs: 'center', md: 'center'},
+                                alignText: {xs: 'center', md: 'center'},
+                            }}>
+                                <Box sx={{textAlign: { xs: 'center', md: 'center' }}}>
+                                    <Typography variant="h6">Card suplimentar de test in Coloana 1</Typography>
+                                </Box>
                             </Paper>
 
                         </Box>
                     </Grid>
 
                     <Grid item xs={12} md={4}>
-                        <Box sx={{flexDirection: 'column', display: 'flex', gap: 3}}>
+                        <Box sx={{display: 'flex', flexDirection: 'column', gap: 3, width: '100%'}}>
                             <Paper
                                 elevation={3}
                                 sx={{
-                                display: 'flex',
-                                backgroundColor: "rgba(31, 99, 135, 0.169)",
-                                color: 'white',
-                                borderRadius: '16px',
-                                padding: '24px',
+                                    width: '100%',
+                                    backgroundColor: "rgba(31, 99, 135, 0.169)",
+                                    color: 'white',
+                                    borderRadius: '16px',
+                                    padding: '24px',
+                                    flexGrow: 1,
                                 }}>
-                                <Box sx={{display: 'flex', flexDirection: {xs: 'column', md: 'row'}, alignItems: {xs: 'center', md: 'center'}, gap: 2
+
+                                <Box sx={{ flexDirection: {xs: 'column', md: 'row'}, alignItems: {xs: 'center', md: 'flex-start'}, gap: 2
                                 }}>
-                                    <Avatar sx={{width: 150, height: 150}}  alt="stai sa se incarce poza..." src="/profile1.jpg"></Avatar>
-                                    <Box sx={{ textAlign: { xs: 'center', md: 'left' } }}>
+                                    <Box sx={{ textAlign: { xs: 'center', md: 'center' } }}>
                                         <Typography variant="h4">
-                                            nenea nenea nenea nenea
+                                            personal space 2
                                         </Typography>
                                         <Typography variant="subtitle1">
-                                            nenea NENEA
+                                            idk how you got here, but thanks for dropping by :D
                                         </Typography>
                                     </Box>
                                 </Box>
                             </Paper>
                         </Box>
                     </Grid>
-
-                    <Grid item xs={12} md={4}>
-                        <Paper
-                            elevation={3}
-                            sx={{
-                                display: 'flex',
-                                backgroundColor: "rgba(31, 99, 135, 0.169)",
-                                color: 'white',
-                                borderRadius: '16px',
-                                padding: '24px',
-                            }}>
-
-                            <Box sx={{display: 'flex', flexDirection: {xs: 'column', md: 'row'}, alignItems: {xs: 'center', md: 'flex-start'}, gap: 2
-                            }}>
-                                <Box sx={{ textAlign: { xs: 'center', md: 'center' } }}>
-                                    <Typography variant="h4">
-                                         personal space 2
-                                    </Typography>
-                                    <Typography variant="subtitle1">
-                                        idk how you got here, but thanks for dropping by :D
-                                    </Typography>
-                                </Box>
-                            </Box>
-                        </Paper>
-                    </Grid>
-
                 </Grid>
             </Box>
             <Footer/>
